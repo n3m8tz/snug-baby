bee9adeb659e620f0fb907692128e7a32f89f292
AndrewSmarty <mav1996@mail.ru>
2015-03-15 00:23:04 -0700

    Snug-Baby 5.0

--- js/gapi.realtime.js
+++ js/gapi.realtime.js
@@ -1,13 +1,16 @@
 	var listDemo;
 	var initialAppStart = false;
+	var realtimeLoader;
+	var DEBUG_MODE = true;
+
 
 	var realtimeOptions = {
 
 		/**
 		* Client ID from the console.
 		*/
-		clientId: '488687976561-cbn0sjmncb567hviggdetb3g84tb6ipk.apps.googleusercontent.com',
- 
+		clientId: '777750276820-30pop6psr99unjqt34ubmrq50fi5ao76.apps.googleusercontent.com',
+
 		/**
 		* The ID of the button to click to authorize. Must be a DOM element ID.
 		*/
@@ -23,7 +26,7 @@
 		/**
 		* Autocreate files right after auth automatically.
 		*/
-		autoCreate: true,
+		autoCreate: false,
 
 		/**
 		* The name of newly created Drive files.
@@ -50,28 +53,22 @@
 		/**
 		* Function to be called after authorization and before loading files.
 		*/
-		afterAuth: null // No action.
+		afterAuth: afterAuth // No action.
 	}
 
-	/*retrieveAllFiles(function(response){
-		console.log(response.toString());
-	});*/
-	
+	//
 	function initializeModel(model){
-		//if( gapi.drive.realtime.Model.isInitialized()){
 			var collaborativeList = model.createList();
 			model.getRoot().set("data_list", collaborativeList);
-		//}
 	}
 
 	/**
 	* Retrieve a list of File resources.
-	* 
+	*
 	* @param {Function} callback Function to call when the request is complete.
 	*/
 
 	function retrieveAllFiles(callback) {
-
 		var retrievePageOfFiles = function(request, result) {
 			request.execute(function(resp) {
 		  		result = result.concat(resp.items);
@@ -86,10 +83,10 @@
 				}
 			});
 		}
-
 		var initialRequest = gapi.client.drive.files.list();
 		retrievePageOfFiles(initialRequest, []);
 	}
+
 
 	function onFileLoaded(doc){
 		var model = doc.getModel();
@@ -97,9 +94,16 @@
 		var array = listDemo.asArray();
 		var length = listDemo.length;
 
-		console.log("ListDemo object: " + listDemo);
-		console.log("array: " + array);
-		console.log("length: " + length);
+		for(var i=0; i< 5; i++){
+			console.log("");
+		}
+		console.log("-----------------------------------------------");
+		console.log("-------------- FULL INFORMATION ---------------");
+		console.log("-----------------------------------------------");
+		console.log("\t" + array);
+		for(var i=0; i< 5; i++){
+			console.log("");
+		}
 
 		var textarea = $("#textarea")[0];
 		textarea.value = array;
@@ -111,11 +115,31 @@
 
 		listDemo.addEventListener(gapi.drive.realtime.EventType.VALUES_ADDED, onListChange);
 		listDemo.addEventListener(gapi.drive.realtime.EventType.VALUES_REMOVED, onListChange);
-		
+
 		onListChange();
 	}
 
-	function startGoogleDriveRealtime(){
-		var realtimeLoader = new rtclient.RealtimeLoader(realtimeOptions);
-      	realtimeLoader.start();
+	function afterAuth () {
+		// only create/load files when no files were loaded initialy
+		gapi.client.load('drive', 'v2', function () {
+			if (rtclient.params.fileIds && rtclient.params.fileIds.length) {
+				return;
+			}
+			retrieveAllFiles(function (files) {
+				if (files.length === 0) {
+					// create new file
+					realtimeLoader.createNewFileAndRedirect();
+				} else {
+					// get last file and use it
+					// TODO: add dialog to select files
+					var file = files[files.length - 1];
+					realtimeLoader.redirectTo([file.id], realtimeLoader.authorizer.userId);
+				}
+			});
+		});
+	}
+
+	function startGoogleDriveRealtime() {
+		realtimeLoader = new rtclient.RealtimeLoader(realtimeOptions);
+		realtimeLoader.start();
 	}
